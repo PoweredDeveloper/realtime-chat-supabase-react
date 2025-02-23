8758b4042fcc51d017a649d3f22f2131b31d41bf
Powered Developer <orokana.otaku.dev@gmail.com>
2025-02-23 19:20:09 +0300

    safdasfas

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://rzotigsojontydgnncqc.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6b3RpZ3Nvam9udHlkZ25uY3FjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAzMjMxMjYsImV4cCI6MjA1NTg5OTEyNn0.YxEi6g3widThF0_gDte9VOjadY61QJzXSbDL2QO42uE';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
