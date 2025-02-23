b73fd2e05bc83f71f8eac56a9426533d27443c24
Powered Developer <orokana.otaku.dev@gmail.com>
2025-02-23 20:43:24 +0300

    fixes

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -31,7 +31,7 @@ export default function MessageForm() {
           username,
           country,
           is_authenticated: session ? true : false,
-          timestamp: new Date().toISOString()
+          timestamp: new Date(new Date() + 360000).toISOString()
         },
       ]);
 
